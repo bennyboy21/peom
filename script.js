2980506df1812c2987f8c722bad0b52cfa26bc17
Benjamin Burnell <70352588+bennyboy21@users.noreply.github.com>
2023-11-14 23:02:44 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -50,15 +50,32 @@ function isBrighter(avgColor) {
 }
 
 function showCreateOption() {
-    // document.getElementById("create-Poem-Container").style.transition = ".5s"
+    document.getElementById("create-Poem-Container").style.transition = ".5s"
     document.getElementById("create-Poem-Container").style.bottom = "0px"
+    document.getElementById("create-Poem-Container").style.top = "0px"
     document.getElementById("background-Shadown").style.zIndex = "10"
-    // document.getElementById("background-Shadown").style.transition = ".5s"
+    document.getElementById("background-Shadown").style.transition = ".5s"
     document.getElementById("background-Shadown").style.opacity = "1"
 
     setTimeout(function() {
         document.getElementById("create-Poem-Container").style.transition = "0s"
         document.getElementById("background-Shadown").style.transition = "0s"
+    })
+}
+
+function closeCreateOption() {
+    document.getElementById("create-Poem-Container").style.transition = ".5s"
+    document.getElementById("create-Poem-Container").style.bottom = "-100vh"
+    document.getElementById("create-Poem-Container").style.top = "100vh"
+    document.getElementById("background-Shadown").style.transition = ".5s"
+    document.getElementById("background-Shadown").style.opacity = "0"
+
+    setTimeout(function() {
+        document.getElementById("create-Poem-Container").style.transition = "0s"
+        document.getElementById("background-Shadown").style.transition = "0s"
+        setTimeout(function() {        
+            document.getElementById("background-Shadown").style.zIndex = "-1"
+        }, 200)
     })
 }
 
@@ -91,41 +108,152 @@ getAverageColor(imageUrl, function (avgColor) {
 
 
 const apiKey = 'pTFdtB7nkvUbpcUtForgIX9NaX7YNgFYFM2QZ2eHbkmLox4u1P6mF5Zk';
-const searchQuery = 'Dark Couple Images'; // Replace with your search query
+var searchQuery = 'Love'; // Replace with your search query
 
-const apiUrl = `https://api.pexels.com/v1/search?query=${searchQuery}&per_page=1&page=3`;
+var imageIndex = 1
 
-fetch(apiUrl, {
-    headers: {
-        Authorization: apiKey,
-    },
-})
-.then(response => response.json())
-.then(data => {
-    // Extract the URL of the random photo
-    const photoUrl = data.photos[0].src.large;
-
-    // Display the photo or do something else with it
-    console.log('Random Photo URL:', photoUrl);
-    document.getElementById("poem-Img").setAttribute("src", photoUrl)
-    getAverageColor(photoUrl, function (avgColor) {
-        document.getElementById("top-Gradient").style.background = "linear-gradient(to bottom, rgb(" + avgColor.r + "," + avgColor.g + "," + avgColor.b + "), white)"
-    });
-
-    // getAverageColor(photoUrl, function (avgColor) {
-    //     var brighter = isBrighter(avgColor);
-    //     if (brighter) {
-    //         document.querySelector(".peom-Title").style.color = "rgb(20, 20, 20)"
-    //         document.querySelector(".poem-First-Little-Text").style.color = "rgb(50, 50, 50)"
-    //         console.log("brighter")
-    //     } else {
-    //         document.querySelector(".peom-Title").style.color = "rgb(250, 250, 250)"
-    //         document.querySelector(".poem-First-Little-Text").style.color = "rgb(220, 220, 220)"
-    //         console.log("darker")
-    //     }
-    // });
+function changeImage() {
+    searchQuery = document.getElementById("poem-Title-Change").value
     
-})
-.catch(error => {
-    console.error('Error fetching data from Pexels API:', error);
-});
+    document.getElementById("camera-Icon").style.transition = ".5s"
+    document.getElementById("camera-Icon").style.opacity = "0"
+
+    setTimeout(function() {
+        document.querySelector(".loader-5").style.transition = ".5s"
+        document.querySelector(".loader-5").style.opacity = "1"
+    }, 500)
+
+    imageIndex+= 1
+    var apiUrl = `https://api.pexels.com/v1/search?query=${searchQuery}&per_page=1&page=` + imageIndex;
+
+    fetch(apiUrl, {
+        headers: {
+            Authorization: apiKey,
+        },
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Extract the URL of the random photo
+        const photoUrl = data.photos[0].src.large;
+
+        // Display the photo or do something else with it
+        console.log('Random Photo URL:', photoUrl);
+        document.getElementById("poem-Img").setAttribute("src", photoUrl)
+        getAverageColor(photoUrl, function (avgColor) {
+            if(!isBrighter(avgColor)) {
+                document.getElementById("poem-Title-Change").style.transition = ".5s"
+                document.getElementById("poem-Title-Change").style.color = "white"
+                const style = document.createElement('style');
+                style.innerHTML = `#poem-Title-Change::placeholder {
+                    color: #rgb(255, 255, 255, 0.75);
+                    font-style: italic;
+                }`;
+
+                // Append the style rule to the document head
+                document.head.appendChild(style);
+                document.getElementById("poem-Title-Change").style.border = "2px solid rgb(255, 255, 255, 0.25)"
+            } else {
+                document.getElementById("poem-Title-Change").style.transition = ".5s"
+                document.getElementById("poem-Title-Change").style.color = "black"
+                
+                document.getElementById("poem-Title-Change").style.transition = ".5s"
+                document.getElementById("poem-Title-Change").style.color = "white"
+                const style = document.createElement('style');
+                style.innerHTML = `#poem-Title-Change::placeholder {
+                    color: #rgb(255, 255, 255, 0.75);
+                    font-style: italic;
+                }`;
+
+                document.getElementById("poem-Title-Change").style.border = "2px solid rgb(0, 0, 0, 0.2)"
+            }
+            document.getElementById("poem-Img-Container").style.border = "7px solid " + "rgb(" + avgColor.r + "," + avgColor.g + "," + avgColor.b + ")"
+            document.getElementById("poem-Title-Change").style.background = "rgb(" + avgColor.r + "," + avgColor.g + "," + avgColor.b + ")"
+            // document.getElementById("top-Gradient").style.background = "radial-gradient(rgb(" + avgColor.r + "," + avgColor.g + "," + avgColor.b + ") 10%, rgb(" + avgColor.r + "," + avgColor.g + "," + avgColor.b + ", 0.5) 15%, white 100%)"
+        });
+
+        // getAverageColor(photoUrl, function (avgColor) {
+        //     var brighter = isBrighter(avgColor);
+        //     if (brighter) {
+        //         document.querySelector(".peom-Title").style.color = "rgb(20, 20, 20)"
+        //         document.querySelector(".poem-First-Little-Text").style.color = "rgb(50, 50, 50)"
+        //         console.log("brighter")
+        //     } else {
+        //         document.querySelector(".peom-Title").style.color = "rgb(250, 250, 250)"
+        //         document.querySelector(".poem-First-Little-Text").style.color = "rgb(220, 220, 220)"
+        //         console.log("darker")
+        //     }
+        // });
+        
+    })
+    .catch(error => {
+        console.error('Error fetching data from Pexels API:', error);
+    });
+}
+
+changeImage()
+
+const poemWords = [
+    "Serene", "Whisper", "Enchant", "Meadow", "Melody",
+    "Tranquil", "Cascade", "Ephemeral", "Celestial", "Luminous",
+    "Ethereal", "Twilight", "Echo", "Sorrow", "Bliss",
+    "Solitude", "Embrace", "Radiant", "Mirage", "Illuminate",
+    "Quiver", "Resonate", "Awe", "Surrender", "Velvet",
+    "Tranquility", "Essence", "Cradle", "Cascade", "Harmony",
+    "Blossom", "Mystical", "Serenade", "Silhouette", "Ponder",
+    "Whimsical", "Journey", "Echoes", "Reverie", "Cascade",
+    "Velvet", "Ethereal", "Silken", "Gossamer", "Glisten",
+    "Enigma", "Nebula", "Quixotic", "Soothe", "Enrapture"
+];
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function isOverlapping(element, otherElements) {
+    const rect1 = element.getBoundingClientRect();
+
+    for (const otherElement of otherElements) {
+        const rect2 = otherElement.getBoundingClientRect();
+
+        if (
+            rect1.right >= rect2.left &&
+            rect1.left <= rect2.right &&
+            rect1.bottom >= rect2.top &&
+            rect1.top <= rect2.bottom
+        ) {
+            return true; // Elements overlap
+        }
+    }
+
+    return false; // No overlap
+}
+
+function createRandomPoemWords(containerId, numWords) {
+    const container = document.getElementById(containerId);
+    const existingWords = [];
+
+    for (let i = 0; i < numWords; i++) {
+        const word = poemWords[getRandomInt(0, poemWords.length - 1)];
+
+        const wordElement = document.createElement("div");
+        wordElement.innerText = word;
+        wordElement.className = "poemWord";
+        
+        wordElement.style.fontSize = `${getRandomInt(12, 24)}px`;
+        wordElement.style.fontWeight = getRandomInt(400, 900);
+
+        let xPos, yPos;
+        if(!isOverlapping(wordElement, existingWords)) {
+            xPos = getRandomInt(0, container.clientWidth - wordElement.clientWidth);
+            yPos = getRandomInt(0, container.clientHeight - wordElement.clientHeight);
+        }
+
+        wordElement.style.left = `${xPos}px`;
+        wordElement.style.top = `${yPos}px`;
+
+        container.appendChild(wordElement);
+        existingWords.push(wordElement);
+    }
+}
+
+createRandomPoemWords("create-Poem-Container", 25);
